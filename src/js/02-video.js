958de80d6b1956686e2e74f49b27e6fe577884b3
Sergey Kabanets <skabanets97@gmail.com>
2023-12-01 20:23:52 +0200

    add task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let videoplayerCurrentTime =
+  localStorage.getItem('videoplayer-current-time') || 0;
+
+player.setCurrentTime(videoplayerCurrentTime);
+
+const onPlay = ({ seconds }) =>
+  localStorage.setItem('videoplayer-current-time', `${Math.floor(seconds)}`);
+
+const setVideoCurrentTime = throttle(onPlay, 1000, {
+  leading: false,
+  trailing: true,
+});
+
+player.on('timeupdate', setVideoCurrentTime);
