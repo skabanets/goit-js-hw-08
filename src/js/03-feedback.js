78b4f0023f1af34d0a27ce4cf27fd238c91c3058
Sergey Kabanets <skabanets97@gmail.com>
2023-12-01 23:49:22 +0200

    fix task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,43 +6,33 @@ const refs = {
   messageInput: document.querySelector('.js-form-message'),
 };
 
-const data = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+let data = JSON.parse(localStorage.getItem('feedback-form-state')) || {
   email: '',
   message: '',
 };
 
-const saveFormData = e => {
-  if (e.target.name === 'email') {
-    data.email = e.target.value;
-  }
-
-  if (e.target.name === 'message') {
-    data.message = e.target.value;
-  }
-
+const hadnleChange = e => {
+  const { name, value } = e.target;
+  data[name] = value;
   const formData = JSON.stringify(data);
   localStorage.setItem('feedback-form-state', formData);
 };
 
-const onSaveDataInput = throttle(saveFormData, 500);
+const hadnleChangeInput = throttle(hadnleChange, 500);
 
 const onFillFormLoad = ({ email, message }) => {
   refs.emailInput.value = email;
   refs.messageInput.value = message;
 };
 
-const cleanForm = () => {
-  refs.emailInput.value = '';
-  refs.messageInput.value = '';
-};
-
 window.addEventListener('load', onFillFormLoad(data));
 
-refs.form.addEventListener('input', onSaveDataInput);
+refs.form.addEventListener('input', hadnleChangeInput);
 
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
   console.log(data);
   localStorage.clear();
-  cleanForm();
+  data = { email: '', message: '' };
+  e.target.reset();
 });
